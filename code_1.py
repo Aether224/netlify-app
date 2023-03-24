6656ea1bfaf54406a22bd7864904611f65cbd82e
unknown <Aether224>
2023-03-24 14:35:35 +0700

    Add new project caculator

---
+++ code_1.py
@@ -0,0 +1,9 @@
+print("Máy tính 4 phép cơ bản")
+a = int(input('Nhập số a: '))
+b = int(input('Nhập số b: '))
+print(f"""
+{a} + {b} = {a+b}
+{a} - {b} = {a-b}
+{a} x {b} = {a*b}
+{a} / {b} = {round(a/b,2)}
+""")
